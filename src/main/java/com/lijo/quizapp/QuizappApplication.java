924b529d3a9e29b5b91563765168e3aeab61f4be
Lijo Joseph <lijojoseph234@gmail.com>
2023-11-17 05:34:17 +0530

    put, delete mapping successful

--- src/main/java/com/lijo/quizapp/QuizappApplication.java
+++ src/main/java/com/lijo/quizapp/QuizappApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizappApplication {
+public class QuizappApplication
+{
 
 	public static void main(String[] args)
 	{
--- src/main/java/com/lijo/quizapp/controller/QuestionController.java
+++ src/main/java/com/lijo/quizapp/controller/QuestionController.java
@@ -16,6 +16,7 @@ public class QuestionController
 
     @Autowired
     QuestionService questionService;
+    // to get all the questions
     @GetMapping("allquestions")
     public List<Question> getAllQuestions()
     {
@@ -23,15 +24,33 @@ public class QuestionController
         return questionService.getAllQuestions();
     }
 
+    // to get category wise result
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category)
     {
         return questionService.getQuestionsByCategory(category);
     }
 
+    // to add records
     @PostMapping("add")
     public String addQuestion(@RequestBody Question question)
     {
         return questionService.addQuestion(question);
     }
+
+    // delete and update controllers:
+    // to delete records
+    @DeleteMapping("delete/{aid}")
+    public String deleteQuestion(@PathVariable ("aid") int aid)
+    {
+        return questionService.deleteQuestion(aid);
+    }
+
+    @PutMapping("update/{aid}")
+    public String updateQuestion(@PathVariable ("aid") int aid, @RequestBody Question question)
+    {
+        questionService.deleteQuestion(aid);
+        return questionService.addQuestion(question);
+    }
+
 }
